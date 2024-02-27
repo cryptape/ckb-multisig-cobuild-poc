f56f3addd569a05921b1323e7513fa5f5debf39a
ian <ian@cryptape.com>
2024-02-27 17:09:29 +0800

    :memo: Document the workflow

--- src/IndexPage.js
+++ src/IndexPage.js
@@ -28,19 +28,73 @@ function AddressesList({ navigate, addresses, deleteAddress }) {
   );
 }
 
+const IMPORT_PAGE = "#/transactions/import";
+
 function TransactionsList({ navigate }) {
   return (
     <section className="mb-8">
       <h2 className="text-lg border-b-2 mb-4">Transactions</h2>
-      <div className="mb-4 flex flex-row gap-2 flex-wrap">
-        <Button onClick={() => navigate("#/transactions/import")}>
+      <div className="mb-8 flex flex-row gap-2 flex-wrap">
+        <Button onClick={() => navigate(IMPORT_PAGE)}>
           Import Transaction
         </Button>
       </div>
-      <p className="text-sm">
-        Importing will automatically merge collected signatures for the same
-        transaction.
-      </p>
+
+      <h3 className="mb-2">The Workflow</h3>
+      <ol className="mb-4 list-decimal list-inside">
+        <li>
+          The Initiator creates the multisig transaction using{" "}
+          <code>ckb-cli tx</code> subcommand, or Neuron multisig address tool
+          found in the Tools menu.
+        </li>
+        <li>
+          The Initiator{" "}
+          <a
+            className="text-blue-600 dark:text-blue-500 hover:underline"
+            href={IMPORT_PAGE}
+          >
+            imports
+          </a>{" "}
+          the transaction JSON file into this web app and review the
+          transaction.
+        </li>
+        <li>
+          The Initiator exports transaction as the building packet JSON and
+          sends it to signers.
+        </li>
+        <li>
+          Signers{" "}
+          <a
+            className="text-blue-600 dark:text-blue-500 hover:underline"
+            href={IMPORT_PAGE}
+          >
+            import
+          </a>{" "}
+          the building packet JSON file into this web app and review the
+          transaction.
+        </li>
+        <li>
+          Signers export the transaction for ckb-cli or Neuron depending on
+          which tools they are using.
+        </li>
+        <li>
+          Signers send the signed JSON file from ckb-cli or Neuron to the
+          Initiator.
+        </li>
+        <li>
+          The Initiator{" "}
+          <a
+            className="text-blue-600 dark:text-blue-500 hover:underline"
+            href={IMPORT_PAGE}
+          >
+            import
+          </a>{" "}
+          the received JSON files from Signers to merge signatures.
+        </li>
+        <li>
+          The Initiator sends the transaction when the threshold is reached.
+        </li>
+      </ol>
     </section>
   );
 }
